f3a7c23ecdfacbcb2ebab80b33da33212044fee2
James Xu <54513773+fbjxu@users.noreply.github.com>
2020-09-25 23:21:34 -0700

    added summary and outlook

--- app.py
+++ app.py
@@ -1,30 +1,36 @@
-from flask import Flask, redirect, render_template, request, url_for, session
+from flask import Flask, request, jsonify
 import requests
+stockName = ''
 
 app = Flask(__name__)
-
-#TODO: corner case when user does not input correct ticker or user do not provide any input
-
-@app.route('/', methods = ["POST", "GET"])
-def hello():
-    responseOutlookDict = {}
-    responseSummaryList = {}
-
-    if request.method == "POST":
-        symbol = request.form['nm']
-        headers = {
+headers = {
             'Content-Type': 'application/json',
             'Authorization' : 'Token a4d9cb249227d4a1c64fac98783787069f17c866'
         }
-        # get request
-        if symbol != "":
-            outlookResponse = requests.get("https://api.tiingo.com/tiingo/daily/"+symbol, headers=headers)
-            summaryResponse = requests.get("https://api.tiingo.com/iex/"+symbol, headers=headers)
-            #convert received request to json
-            responseOutlookDict = outlookResponse.json()  
-            responseSummaryList = summaryResponse.json()[0]
-    return render_template("hello.html", outlook = responseOutlookDict, summary = responseSummaryList)
+outlookResponse = {}
 
+@app.route('/', methods = ["POST", "GET"])
+def index():
+    return app.send_static_file("index.html")
+
+@app.route('/api/stockName', methods = ["GET"])
+def stockName():
+    symbol = request.args.get('symbol', default = "msft")
+    return symbol
+
+@app.route('/api/outlook', methods = ["GET"])
+def outlook():
+    symbol = request.args.get('symbol', default = "undetermined")
+    if symbol != "":
+        outlookResponse = requests.get("https://api.tiingo.com/tiingo/daily/"+symbol, headers=headers)
+    return jsonify(outlookResponse.json())
+
+@app.route('/api/summary', methods = ["GET"])
+def summary():
+    symbol = request.args.get('symbol', default = "undetermined")
+    if symbol != "":
+        summaryResponse = requests.get("https://api.tiingo.com/iex/"+symbol+"?token=a4d9cb249227d4a1c64fac98783787069f17c866")
+    return jsonify(summaryResponse.json())
 
 if __name__ == "__main__":
     app.run()
