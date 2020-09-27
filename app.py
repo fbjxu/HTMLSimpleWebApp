ba6c865e617ae6beb5723679335149e19a2ff058
James Xu <54513773+fbjxu@users.noreply.github.com>
2020-09-26 23:29:02 -0700

    added graph

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
+import json
+from pytz import timezone, utc
+import datetime
+from dateutil.relativedelta import relativedelta
 import requests
 stockName = ''
 
+token = "a4d9cb249227d4a1c64fac98783787069f17c866"
 app = Flask(__name__)
 headers = {
             'Content-Type': 'application/json',
@@ -16,7 +21,11 @@ def index():
 @app.route('/api/stockName', methods = ["GET"])
 def stockName():
     symbol = request.args.get('symbol', default = "msft")
-    return symbol
+    now = datetime.datetime.now(tz=utc) # get current time
+    now = now.astimezone(timezone('US/Pacific')) # set now to PST
+    today_date = f"{now.year}-{now.month}-{now.day}"
+    res = [symbol, today_date]
+    return json.dumps(res)
 
 @app.route('/api/outlook', methods = ["GET"])
 def outlook():
@@ -31,6 +40,17 @@ def summary():
     if symbol != "":
         summaryResponse = requests.get("https://api.tiingo.com/iex/"+symbol+"?token=a4d9cb249227d4a1c64fac98783787069f17c866")
     return jsonify(summaryResponse.json())
+
+@app.route('/api/chart', methods = ["GET"])
+def chart():
+    symbol = request.args.get('symbol', default = "undetermined")
+    if symbol != "":
+        now = datetime.datetime.now(tz=utc) # get current time
+        now = now.astimezone(timezone('US/Pacific')) # set now to PST
+        startDate_date = (now + relativedelta(months=-6))
+        startDate = f"{startDate_date.year}-{startDate_date.month}-{startDate_date.day}"
+        chartResponse = requests.get("https://api.tiingo.com/iex/"+symbol+"/prices?token="+token+"&startDate="+startDate+"&resampleFreq=12hour&columns=open,high,low,close,volume")
+    return jsonify(chartResponse.json())
 
 if __name__ == "__main__":
     app.run()
