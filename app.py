a06e759b1160fb60aff9d5065b88895387626a28
James Xu <54513773+fbjxu@users.noreply.github.com>
2020-09-28 09:27:32 -0700

    with css

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 from pytz import timezone, utc
 import datetime
 from dateutil.relativedelta import relativedelta
 import requests
-stockName = ''
+application = Flask(__name__)
+CORS(application)
 
 token = "a4d9cb249227d4a1c64fac98783787069f17c866"
-app = Flask(__name__)
+
 headers = {
             'Content-Type': 'application/json',
             'Authorization' : 'Token a4d9cb249227d4a1c64fac98783787069f17c866'
         }
 outlookResponse = {}
 
-@app.route('/', methods = ["POST", "GET"])
+@application.route('/', methods = ["POST", "GET"])
 def index():
-    return app.send_static_file("index.html")
+    return application.send_static_file("index.html")
 
-@app.route('/api/stockName', methods = ["GET"])
+@application.route('/api/stockName', methods = ["GET"])
 def stockName():
     symbol = request.args.get('symbol', default = "msft")
     now = datetime.datetime.now(tz=utc) # get current time
@@ -27,21 +29,21 @@ def stockName():
     res = [symbol, today_date]
     return json.dumps(res)
 
-@app.route('/api/outlook', methods = ["GET"])
+@application.route('/api/outlook', methods = ["GET"])
 def outlook():
     symbol = request.args.get('symbol', default = "undetermined")
     if symbol != "":
         outlookResponse = requests.get("https://api.tiingo.com/tiingo/daily/"+symbol, headers=headers)
     return jsonify(outlookResponse.json())
 
-@app.route('/api/summary', methods = ["GET"])
+@application.route('/api/summary', methods = ["GET"])
 def summary():
     symbol = request.args.get('symbol', default = "undetermined")
     if symbol != "":
         summaryResponse = requests.get("https://api.tiingo.com/iex/"+symbol+"?token=a4d9cb249227d4a1c64fac98783787069f17c866")
     return jsonify(summaryResponse.json())
 
-@app.route('/api/chart', methods = ["GET"])
+@application.route('/api/chart', methods = ["GET"])
 def chart():
     symbol = request.args.get('symbol', default = "undetermined")
     if symbol != "":
@@ -53,4 +55,4 @@ def chart():
     return jsonify(chartResponse.json())
 
 if __name__ == "__main__":
-    app.run()
+    application.run(debug=True)
