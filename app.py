695b41db938334752fa8a0b23ed218fa5be3b7c6
James Xu <54513773+fbjxu@users.noreply.github.com>
2020-09-29 10:38:13 -0700

    still need green/red arrows

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ application = Flask(__name__)
 CORS(application)
 
 token = "a4d9cb249227d4a1c64fac98783787069f17c866"
-
+newstoken = "fd84f96ea1e248a29d8fa184296cdb8f"
 headers = {
             'Content-Type': 'application/json',
             'Authorization' : 'Token a4d9cb249227d4a1c64fac98783787069f17c866'
@@ -26,7 +26,11 @@ def stockName():
     now = datetime.datetime.now(tz=utc) # get current time
     now = now.astimezone(timezone('US/Pacific')) # set now to PST
     today_date = f"{now.year}-{now.month}-{now.day}"
-    res = [symbol, today_date]
+    validResponse = (requests.get("https://api.tiingo.com/tiingo/daily/"+symbol, headers=headers)).json()
+    valid = "no"
+    if("detail" not in validResponse):
+        valid = "yes"
+    res = [symbol, today_date, valid]
     return json.dumps(res)
 
 @application.route('/api/outlook', methods = ["GET"])
@@ -54,5 +58,13 @@ def chart():
         chartResponse = requests.get("https://api.tiingo.com/iex/"+symbol+"/prices?token="+token+"&startDate="+startDate+"&resampleFreq=12hour&columns=open,high,low,close,volume")
     return jsonify(chartResponse.json())
 
+@application.route('/api/news', methods = ["GET"])
+def news():
+    symbol = request.args.get('symbol', default = "undetermined")
+    if symbol != "":
+        newsResponse = requests.get("https://newsapi.org/v2/everything?q="+symbol+"&apiKey="+newstoken)
+    res = newsResponse.json()["articles"]
+    return jsonify(res)
+
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run()
